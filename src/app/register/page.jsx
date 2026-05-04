67ee1f71b00694c76a0706f831e1dc585ae2f55d
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-05-04 12:23:38 +0600

    login and register pages added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
 
-const LoginPage = () => {
+const RegisterPage = () => {
     return (
         <div className="bg-slate-950/90 space-y-20 py-15">
             <div className="mx-auto w-full max-w-md px-4 py-12">
                 <div className="rounded-2xl border border-white/10 bg-slate-900 p-6 md:p-8">
-                    <h1 className="text-2xl font-bold">Login</h1>
+                    <h1 className="text-2xl font-bold">Register</h1>
                     <form className="mt-6 space-y-4" >
+                        <input
+                            className="input input-bordered w-full bg-slate-800"
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            required
+                            
+                        />
                         <input
                             className="input input-bordered w-full bg-slate-800"
                             type="email"
@@ -18,6 +26,14 @@ const LoginPage = () => {
                         />
                         <input
                             className="input input-bordered w-full bg-slate-800"
+                            type="url"
+                            name="image"
+                            placeholder="Photo URL"
+                            required
+                       
+                        />
+                        <input
+                            className="input input-bordered w-full bg-slate-800"
                             type="password"
                             name="password"
                             placeholder="Password"
@@ -25,18 +41,18 @@ const LoginPage = () => {
                            
                         />
                         <button className="btn btn-info w-full"  type="submit">
-                            {/* {loading ? "Logging in..." : "Login"} */}Login
+                            {/* {loading ? "Creating account..." : "Register"} */}Register
                         </button>
                     </form>
 
-                    <button className="btn btn-outline mt-3 w-full" type="button">
+                    <button className="btn btn-outline mt-3 w-full"  type="button">
                         Continue with Google
                     </button>
 
                     <p className="mt-4 text-sm text-slate-300">
-                        New here?{" "}
-                        <Link href="/register" className="link link-info">
-                            Register
+                        Already have an account?{" "}
+                        <Link href="/login" className="link link-info">
+                            Login
                         </Link>
                     </p>
                 </div>
@@ -45,4 +61,4 @@ const LoginPage = () => {
     );
 };
 
-export default LoginPage;
+export default RegisterPage;
