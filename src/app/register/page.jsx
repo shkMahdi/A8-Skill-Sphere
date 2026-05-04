5c41b0c63e682b4af9b5d540e6b360337dc8e43f
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-05-04 14:53:32 +0600

    email password auth with mongodb setup done

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,13 +1,35 @@
 "use client";
+import { authClient } from '@/lib/auth-client';
 import Link from 'next/link';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 const RegisterPage = () => {
-    const {register, handleSubmit} = useForm();
-    
-    const handleRegister = (data) => {
+    const { register, handleSubmit } = useForm();
+
+    const handleRegister = async (data) => {
         console.log(data);
+
+        const { name, email, password, image } = data;
+
+        const { data: res, error } = await authClient.signUp.email({
+            name: name, // required
+            email: email, // required
+            password: password, // required
+            image: image,
+            callbackURL: "/",
+        });
+
+        console.log(res, error);
+
+        if(error) {
+            toast.error(error.message);
+        }
+
+        if(res) {
+            toast.success("Account created successfully!");
+        }
     }
     return (
         <div className="bg-slate-950/90 space-y-20 py-15">
@@ -18,10 +40,10 @@ const RegisterPage = () => {
                         <input
                             className="input input-bordered w-full bg-slate-800"
                             type="text"
-                            {...register("name")}       
+                            {...register("name")}
                             placeholder="Name"
                             required
-                            
+
                         />
                         <input
                             className="input input-bordered w-full bg-slate-800"
@@ -29,7 +51,7 @@ const RegisterPage = () => {
                             {...register("email")}
                             placeholder="Email"
                             required
-                           
+
                         />
                         <input
                             className="input input-bordered w-full bg-slate-800"
@@ -37,7 +59,7 @@ const RegisterPage = () => {
                             {...register("image")}
                             placeholder="Photo URL"
                             required
-                       
+
                         />
                         <input
                             className="input input-bordered w-full bg-slate-800"
@@ -45,14 +67,14 @@ const RegisterPage = () => {
                             {...register("password")}
                             placeholder="Password"
                             required
-                           
+
                         />
-                        <button className="btn btn-info w-full"  type="submit">
+                        <button className="btn btn-info w-full" type="submit">
                             {/* {loading ? "Creating account..." : "Register"} */}Register
                         </button>
                     </form>
 
-                    <button className="btn btn-outline mt-3 w-full"  type="button">
+                    <button className="btn btn-outline mt-3 w-full" type="button">
                         Continue with Google
                     </button>
 
